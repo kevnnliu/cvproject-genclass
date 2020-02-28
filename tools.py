f2e5f2948c133dc4fd7c203d456dde39eda44ea6
Kevin Liu <kdliu00@gmail.com>
2020-02-27 17:27:55 -0800

    Added functions for preparing and loading the image data

---
+++ tools.py
@@ -0,0 +1,134 @@
+import pathlib
+import numpy as np
+import cv2
+import pandas
+import matplotlib as plt
+import os
+import random
+
+def prepare_data():
+    print('Preparing data\n')
+    image_folder = 'data/tiny-imagenet-200/'
+    class_labels = {}
+    index = 0
+    print('Loading labels\n')
+    with open(image_folder + 'wnids.txt', 'r') as labels_file:
+        for line in labels_file:
+            label = line.strip()
+            class_labels[label] = index
+            index += 1
+    print('Done\n')
+
+    train_folder = image_folder + 'train/'
+    val_folder = image_folder + 'val/'
+    test_folder = image_folder + 'test/'
+
+    train_images = []
+
+    print('Loading training images\n')
+    for label in class_labels.keys():
+        class_folder_train = train_folder + label + '/images/'
+        train_images.extend([class_folder_train + '{}'.format(i) for i in os.listdir(class_folder_train)])
+    print('Done\n')
+
+    print('Loading validation images\n')
+    val_images = [val_folder + 'images/{}'.format(i) for i in os.listdir(val_folder + 'images/')]
+    print('Done\n')
+
+    print('Loading test images\n')
+    test_images = [test_folder + 'images/{}'.format(i) for i in os.listdir(test_folder + 'images/')]
+    print('Done\n')
+
+    print('Shuffling\n')
+    random.seed(91387264)
+    random.shuffle(train_images)
+    random.shuffle(val_images)
+    random.shuffle(test_images)
+    print('Done\n')
+
+    X_train = []
+    y_train = []
+
+    X_val = []
+    y_val =[]
+
+    X_test = []
+    # No need for y_test
+
+    ROWS = 64
+    COLUMNS = 64
+
+    print('Formatting training images\n')
+    for image in train_images:
+        X_train.append(cv2.resize(cv2.imread(image, cv2.IMREAD_COLOR), (ROWS, COLUMNS), interpolation=cv2.INTER_CUBIC))
+        label = image[29 : 38]
+        encoding = np.zeros(200)
+        encoding[class_labels[label]] = 1
+        y_train.append(encoding)
+    print('Done\n')
+
+    print('Getting validation labels\n')
+    val_labels = {}
+    with open(val_folder + 'val_annotations.txt', 'r') as val_labels_file:
+        for line in val_labels_file:
+            line = line.strip().split()
+            val_labels[line[0]] = line[1]
+    print('Done\n')
+
+    print('Formatting validation images\n')
+    for image in val_images:
+        X_val.append(cv2.resize(cv2.imread(image, cv2.IMREAD_COLOR), (ROWS, COLUMNS), interpolation=cv2.INTER_CUBIC))
+        actual_image = image[34 :]
+        label = val_labels[actual_image]
+        encoding = np.zeros(200)
+        encoding[class_labels[label]] = 1
+        y_val.append(encoding)
+    print('Done\n')
+
+    print('Formatting test images\n')
+    for image in test_images:
+        X_test.append(cv2.resize(cv2.imread(image, cv2.IMREAD_COLOR), (ROWS, COLUMNS), interpolation=cv2.INTER_CUBIC))
+    print('Done\n')
+
+    print('Converting to numpy arrays\n')
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+
+    X_val = np.array(X_val)
+    y_val = np.array(y_val)
+
+    X_test = np.array(X_test)
+    # No need for y_test
+    print('Done\n')
+
+    print('Saving numpy arrays\n')
+    np.save(image_folder + 'X_train', X_train)
+    np.save(image_folder + 'y_train', y_train)
+
+    np.save(image_folder + 'X_val', X_val)
+    np.save(image_folder + 'y_val', y_val)
+
+    np.save(image_folder + 'X_test', X_test)
+    print('Done\n')
+
+    print('Finished preparing data\n')
+
+def load_data(dataset='train'):
+    print('Loading data\n')
+    image_folder = 'data/tiny-imagenet-200/'
+    X = None
+    y = None
+    if dataset == 'train':
+        X = np.load(image_folder + 'X_train.npy')
+        y = np.load(image_folder + 'y_train.npy')
+    elif dataset == 'val':
+        X = np.load(image_folder + 'X_val.npy')
+        y = np.load(image_folder + 'y_val.npy')
+    elif dataset == 'test':
+        X = np.load(image_folder + 'X_test.npy')
+    else:
+        raise ValueError("Dataset must be either 'train', 'val' or 'test'\n")
+    
+    print('Finished loading data\n')
+
+    return X, y
--- train.py
+++ train.py
@@ -1,24 +1,6 @@
 import tensorflow as tf
 from keras import backend as kb
 
-import pathlib
-import numpy as np
-import cv2
-import pandas
-import matplotlib as plt
-import os
-import random
-
 # Use these to test check for gpu availability
-# tf.test.is_built_with_cuda()
-# tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None)
-
-class_labels = []
-with open('data/tiny-imagenet-200/wnids.txt', 'r') as labels_file:
-    line = labels_file.readline()
-    while line:
-        class_labels.append(line.strip())
-        line = labels_file.readline()
-
-for c in class_labels:
-    print(c)
+tf.test.is_built_with_cuda()
+tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None)
